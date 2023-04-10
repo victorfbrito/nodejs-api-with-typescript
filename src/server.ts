05074776479c40b492a041f49e2c6b4524112ed1
Victor Brito <victor.brito@kumulus.com.br>
2023-04-10 18:54:56 -0300

    feat: debugging config

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,16 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ message: 'hello world' }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'hello world' });
+});
+
+app.post('/courses', (req, res) => {
+  const { name } = req.body;
+
+  return res.json({ name });
+});
 
 app.listen(3333, () => console.log('Server is running!'));
